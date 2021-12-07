59d1f422278586f01f452de8b77054fb65703765
Sarika <sarikachandak@gmail.com>
2021-12-07 14:40:38 +0530

    terrafrom changes

--- c1-versions.tf
+++ c1-versions.tf
@@ -12,6 +12,7 @@ terraform {
 # Provider Block
 provider "aws" {
   region  = var.aws_region
+  shared_credentails_file = "Users/sarik/aws/creds"
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
